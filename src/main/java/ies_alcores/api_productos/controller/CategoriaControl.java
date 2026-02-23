3f226b23cd6946afdbdbb3b5954e4a17af7e12bf
Algalgom <agalgom1303@g.educaand.es>
2026-02-23 09:46:27 +0100

    Subida Listar categorias

---
+++ src/main/java/ies_alcores/api_productos/controller/CategoriaControl.java
@@ -0,0 +1,25 @@
+package ies_alcores.api_productos.controller;
+
+
+import ies_alcores.api_productos.model.Categoria;
+import ies_alcores.api_productos.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categoria")
+public class CategoriaControl {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listar(){
+        return ResponseEntity.ok(this.categoriaService.findAll());
+    }
+}
